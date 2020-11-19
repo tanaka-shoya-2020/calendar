0450ca3bdfa8afcd0cffdaf44063ad15261c8482
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-11-20 01:27:03 +0900

    チーム管理機能、ユーザー管理機能の導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :teams
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :teams, controllers: {
+    sessions:      'teams/sessions',
+    passwords:     'teams/passwords',
+    registrations: 'teams/registrations'
+  }
+
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+  root to: "calendars#index"
 end
