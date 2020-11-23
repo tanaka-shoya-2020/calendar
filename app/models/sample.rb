b262cb950de27eebf089eb479b2e2b31353befe1
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-11-23 15:31:11 +0900

    エラーの表記内容を変更

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -3,7 +3,7 @@ class Sample < ApplicationRecord
   validate :start_end_check
 
   def start_end_check
-    errors.add(:end_time,"の日付を正しく入力してください") unless
+    errors.add(:end_time,"は開始時刻と同じかそれより後の時刻でなければいけません") unless
     self.start_time <= self.end_time
   end
 end
