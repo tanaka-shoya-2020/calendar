380806e7809e3b3291f49add2ebcfa2ece42475a
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-11-23 22:24:36 +0900

    ビューファイルの修正に伴う結合テストの変更

--- spec/support/sample_create_support.rb
+++ spec/support/sample_create_support.rb
@@ -2,19 +2,19 @@ module SampleCreateSupport
   def sample_create(sample)
     visit new_sample_path
     expect(current_path).to eq new_sample_path
-    fill_in 'title', with: sample.title
+    fill_in 'sample_title', with: sample.title
     # 開始時刻の選択
-    find('#_start_time_1i').find("option[value='2020']").select_option
-    find('#_start_time_2i').find("option[value='11']").select_option
-    find('#_start_time_3i').find("option[value='25']").select_option
-    find('#_start_time_4i').find("option[value='12']").select_option
-    find('#_start_time_5i').find("option[value='00']").select_option
+    find('#sample_start_time_1i').find("option[value='2020']").select_option
+    find('#sample_start_time_2i').find("option[value='11']").select_option
+    find('#sample_start_time_3i').find("option[value='25']").select_option
+    find('#sample_start_time_4i').find("option[value='12']").select_option
+    find('#sample_start_time_5i').find("option[value='00']").select_option
     # 終了時刻の選択
-    find('#_end_time_4i').find("option[value='13']").select_option
-    find('#_end_time_5i').find("option[value='00']").select_option
+    find('#sample_end_time_4i').find("option[value='13']").select_option
+    find('#sample_end_time_5i').find("option[value='00']").select_option
     sleep 1
     # 詳細の入力
-    fill_in 'body', with: @sample.body
+    fill_in 'sample_body', with: @sample.body
     # 作成するボタンをクリックするとUserEventモデルのカウント数が1増えることを確認する
     expect { find('input.btn-black').click }.to change { Sample.count }.by(1)
     # カレンダー画面に遷移していることを確認する
--- spec/system/samples_spec.rb
+++ spec/system/samples_spec.rb
@@ -15,19 +15,19 @@ RSpec.describe '予定作成機能', type: :system do
       # 予定作成画面へ遷移したことを確認する
       expect(current_path).to eq new_sample_path
       # タイトルを入力
-      fill_in 'title', with: @sample.title
+      fill_in 'sample_title', with: @sample.title
       # 開始時刻の選択
-      find('#_start_time_1i').find("option[value='2020']").select_option
-      find('#_start_time_2i').find("option[value='11']").select_option
-      find('#_start_time_3i').find("option[value='25']").select_option
-      find('#_start_time_4i').find("option[value='12']").select_option
-      find('#_start_time_5i').find("option[value='00']").select_option
+      find('#sample_start_time_1i').find("option[value='2020']").select_option
+      find('#sample_start_time_2i').find("option[value='11']").select_option
+      find('#sample_start_time_3i').find("option[value='25']").select_option
+      find('#sample_start_time_4i').find("option[value='12']").select_option
+      find('#sample_start_time_5i').find("option[value='00']").select_option
       # 終了時刻の選択
-      find('#_end_time_4i').find("option[value='13']").select_option
-      find('#_end_time_5i').find("option[value='00']").select_option
+      find('#sample_end_time_4i').find("option[value='13']").select_option
+      find('#sample_end_time_5i').find("option[value='00']").select_option
       sleep 1
       # 詳細の入力
-      fill_in 'body', with: @sample.body
+      fill_in 'sample_body', with: @sample.body
       # 作成するボタンをクリックするとUserEventモデルのカウント数が1増えることを確認する
       expect { find('input.btn-black').click }.to change { Sample.count }.by(1)
       # カレンダー画面に遷移していることを確認する
@@ -46,7 +46,7 @@ RSpec.describe '予定作成機能', type: :system do
       # 予定作成画面へ遷移したことを確認する
       expect(current_path).to eq new_sample_path
       # 不正な情報を入力
-      fill_in 'title', with: ''
+      fill_in 'sample_title', with: ''
       # 作成するボタンをクリックしてもUserEventモデルのカウント数が増えないことを確認する
       expect { find('input.btn-black').click }.to change { Sample.count }.by(0)
       # カレンダー画面に遷移せず、作成ページに戻されることを確認する
