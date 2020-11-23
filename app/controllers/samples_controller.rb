7c224d52309ef8973b82b36fdafb1ea22aa92321
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-11-23 10:54:24 +0900

    サンプル機能の導入

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,7 +1,38 @@
 class SamplesController < ApplicationController
   def index
+    @events = Sample.all
   end
 
   def show
   end
+
+  def new
+    @event = Sample.new
+  end
+
+  def create
+    @event = Sample.new(event_params)
+    if @event.valid?
+      @event.save
+      redirect_to samples_path
+    else
+      flash[:alert] = 'タイトルは50文字以内で入力してください'
+      render 'samples/new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def event_params
+      params.permit(:title, :start_time, :end_time, :body)
+    end
 end
