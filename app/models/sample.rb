659c9ddecd66edce7bcbc000bb058c33154fa1df
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-11-23 15:26:31 +0900

    datetime型のバリデーションを追加

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,3 +1,9 @@
 class Sample < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
+  validate :start_end_check
+
+  def start_end_check
+    errors.add(:end_time,"の日付を正しく入力してください") unless
+    self.start_time <= self.end_time
+  end
 end
