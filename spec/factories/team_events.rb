4adcf4471553baeb60989c77fa3edf3f793701fe
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-11-28 14:29:27 +0900

    FactoryBotの入力情報の変更

--- spec/factories/team_events.rb
+++ spec/factories/team_events.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :team_event do
     title { Faker::Lorem.word }
-    start_time { '2020-11-22 05:57:00' }
+    date = Date.today
+    start_time { "#{date} 05:57:00" }
     body { Faker::Lorem.sentence }
     association :team
   end
--- spec/factories/user_events.rb
+++ spec/factories/user_events.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :user_event do
     title { Faker::Lorem.word }
-    start_time { '2020-11-22 05:57:00' }
+    date = Date.today
+    start_time { "#{date} 05:57:00" }
     body { Faker::Lorem.sentence }
     association :user
   end
